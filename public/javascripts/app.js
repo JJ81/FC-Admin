cb6a32b02b689ab82c13b47a21bc955c1ed67c26
ParkSeokje <molla4455@gmail.com>
2017-02-28 13:46:29 +0900

    최적화중

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -5,8 +5,8 @@
 
 requirejs.config({
   paths: {
-    'jquery': ['/vendor/plugins/jQuery/jquery-2.2.3.min'],
-    'jquery-private': ['/javascripts/jquery-private'],
+    'jquery': ['static/vendor/plugins/jQuery/jquery-2.2.3.min'],
+    'jquery-private': ['static/javascripts/jquery-private'],
     // Add this map config in addition to any baseUrl or
     // paths config you may already have in the project.
     map: {
@@ -19,32 +19,32 @@ requirejs.config({
       // be an unresolvable cyclic dependency.
       'jquery-private': { 'jquery': 'jquery' }
     },
-    'jquery_ui': '/vendor/plugins/jQueryUI/jquery-ui.min',
-    'bootstrap': ['/vendor/bootstrap/js/bootstrap.min'],
-    'jszip': '/vendor/plugins/jszip/dist/jszip',
-    'datatables.net': '/vendor/plugins/datatables.net/js/jquery.dataTables',
-    'datatables.net-bs': '/vendor/plugins/datatables.net-bs/js/dataTables.bootstrap',
-    'datatables.net-buttons': '/vendor/plugins/datatables.net-buttons/js/dataTables.buttons',
-    'datatables.net-responsive': '/vendor/plugins/datatables.net-responsive/js/dataTables.responsive',
-    'moment': '/vendor/plugins/moment/moment.2.11.2.min',
-    'moment_ko': '/vendor/plugins/moment/locale/kr',
-    'daterangepicker': '/vendor/plugins/daterangepicker/daterangepicker',
-    'bootstrap_datepicker': '/vendor/plugins/datepicker/bootstrap-datepicker',
-    'excellentExport': '/vendor/plugins/ExcellentExport/excellentexport',
-    'select2': '/vendor/plugins/select2/select2.full.min',
-    'fastclick': '/vendor/plugins/fastclick/fastclick',
-    'adminLTE': '/vendor/dist/js/app.min',
-    'jqueryCookie': '/vendor/plugins/jquery_cookie/jquery.cookie.1.4.1',
-    'jqueryValidate': '/vendor/plugins/jquery_validate/jquery.validate.min',
-    'common': '/javascripts/common',
+    'jquery_ui': 'static/vendor/plugins/jQueryUI/jquery-ui.min',
+    'bootstrap': ['static/vendor/bootstrap/js/bootstrap.min'],
+    'jszip': 'static/vendor/plugins/jszip/dist/jszip',
+    'datatables.net': 'static/vendor/plugins/datatables.net/js/jquery.dataTables',
+    'datatables.net-bs': 'static/vendor/plugins/datatables.net-bs/js/dataTables.bootstrap',
+    'datatables.net-buttons': 'static/vendor/plugins/datatables.net-buttons/js/dataTables.buttons',
+    'datatables.net-responsive': 'static/vendor/plugins/datatables.net-responsive/js/dataTables.responsive',
+    'moment': 'static/vendor/plugins/moment/moment.2.11.2.min',
+    'moment_ko': 'static/vendor/plugins/moment/locale/kr',
+    'daterangepicker': 'static/vendor/plugins/daterangepicker/daterangepicker',
+    'bootstrap_datepicker': 'static/vendor/plugins/datepicker/bootstrap-datepicker',
+    'excellentExport': 'static/vendor/plugins/ExcellentExport/excellentexport',
+    'select2': 'static/vendor/plugins/select2/select2.full.min',
+    'fastclick': 'static/vendor/plugins/fastclick/fastclick',
+    'adminLTE': 'static/vendor/dist/js/app.min',
+    'jqueryCookie': 'static/vendor/plugins/jquery_cookie/jquery.cookie.1.4.1',
+    'jqueryValidate': 'static/vendor/plugins/jquery_validate/jquery.validate.min',
+    'common': 'static/javascripts/common',
     'axios': 'https://unpkg.com/axios/dist/axios.min',
-    'quiz_component': ['/javascripts/components/quiz_component'],
-    'quiz_service': ['/javascripts/components/quiz_service'],
-    'slimScroll': '/vendor/plugins/slimScroll/jquery.slimscroll.min',
-    'bootstrap_datetimepicker': ['/vendor/plugins/bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min'],
-    'es6-promise': '/vendor/plugins/es6-promise/dist/es6-promise',
-    'Vimeo': '/vendor/plugins/vimeo-player-js/dist/player',
-    'tinymce': '/vendor/plugins/tinymce/tinymce.min'
+    'quiz_component': ['static/javascripts/components/quiz_component'],
+    'quiz_service': ['static/javascripts/components/quiz_service'],
+    'slimScroll': 'static/vendor/plugins/slimScroll/jquery.slimscroll.min',
+    'bootstrap_datetimepicker': ['static/vendor/plugins/bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min'],
+    'es6-promise': 'static/vendor/plugins/es6-promise/dist/es6-promise',
+    'Vimeo': 'static/vendor/plugins/vimeo-player-js/dist/player',
+    'tinymce': 'static/vendor/plugins/tinymce/tinymce.min'
   },
   shim: {
     'quiz_component': {
