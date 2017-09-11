2c990e19eac9c87af768a843dfff2b24e88fb022
ParkSeokje <molla4455@gmail.com>
2017-09-11 20:18:58 +0900

    아쿠아플레이어 적용중..

--- public/javascripts/components/aquaplayer_service.js
+++ public/javascripts/components/aquaplayer_service.js
@@ -42,8 +42,15 @@ window.define([
       // self.options.container.html(NplayerWrapperTemplate);
       // window.$('#video').height(window.$(window).height() - window.$('.wrapper_foot').height());
     },
-    // Player 초기화
     initPlayer: function () {
+      if ('ActiveXObject' in window) {
+        self.initPlayerWindow();
+      } else {
+        self.initPlayerHTML();
+      }
+    },
+    // Player 초기화
+    initPlayerHTML: function () {
       player = new window.NPlayer('video', {
         controlBox: 'nplayer_control.html',
         visible: false,
@@ -115,6 +122,112 @@ window.define([
       player.bindEvent('Error', function (ec) {
         self.reportMessage('Error - ' + ec);
       });
+    },
+    initPlayerWindow: function () {
+      player = new window.NPlayer('video', {
+        controlBox: 'nplayer_control.html',
+        visible: false,
+        mode: 'html5'
+      });
+
+      player.setWatermarkText('watermark');
+
+      window.player = player;
+
+      window.initNPlayerUI(player);
+
+      player.bindEvent('Ready', function () {
+        self.reportMessage('Ready');
+
+        if (window.loadAquaAxPlugin()) {
+          player.setCDNAuthParam(encodedParam);
+          player.addContextMenu('SystemInfo', 'sysinfo');
+
+          player.open({
+            'URL': encodeURI(self.options.fileURL),
+            'Subtitles': [{
+              'URL': encodeURI('')
+            }]
+          });
+          window.setAxPlugin = window.setAquaAxPlugin(self.options.fileURL, encodedParam);
+        }
+      });
+
+      player.bindEvent('OpenStateChanged', function (state) {
+        self.reportMessage('OpenStateChanged');
+
+        switch (state) {
+        case window.NPlayer.OpenState.Opened:
+          player.setVisible(true);
+          break;
+        case window.NPlayer.OpenState.Closed:
+          break;
+        }
+      });
+
+      player.bindEvent('PlayStateChanged', function (state) {
+        self.reportMessage('PlayStateChanged');
+
+        switch (state) {
+        case window.NPlayer.PlayState.Playing:
+          player.setVisible(true);
+
+          if (window.isDup === true) {
+            player.stop();
+            window.alert(window.NPLAYER_DUP_MSG);
+            break;
+          }
+
+          if (window.setAxPlugin === true) {
+            window.AquaAxPlugin.PlayState = window.NPlayer.PlayState.Playing;
+            window.AquaAxPlugin.OpenStateChange();
+          }
+          break;
+
+        case window.NPlayer.PlayState.Stopped:
+          player.setVisible(false);
+          break;
+
+        case window.NPlayer.PlayState.Paused:
+          player.setVisible(true);
+
+          if (window.setAxPlugin === true) {
+            window.AquaAxPlugin.PlayState = window.NPlayer.PlayState.Paused;
+            window.AquaAxPlugin.OpenStateChange();
+          }
+          break;
+        }
+      });
+
+      player.bindEvent('GuardCallback', function (name, desc) {
+        if (window.setAxPlugin === true) window.AquaAxPlugin.SendPVLog(name, desc);
+      });
+
+      player.bindEvent('Error', function (ec) {
+        $('#video').css('background-image', 'none');
+      });
+
+      player.bindEvent('ContextMenu', function (val) {
+        if (val === 'sysinfo') {
+          if (window.setAxPlugin === true) {
+            window.AquaAxPlugin.LoadSystemInfomation();
+            window.alert(
+              'CPU 정보 : ' + window.AquaAxPlugin.DeviceCPU + '\n' +
+              'MEM 정보 : ' + window.AquaAxPlugin.DeviceMem + '\n' +
+              'GPU 정보 : ' + window.AquaAxPlugin.DeviceGPU + '\n\n' +
+              'IP 정보 : ' + window.AquaAxPlugin.DeviceIP + '\n' +
+              'Mac 정보 : ' + window.AquaAxPlugin.DeviceMac + '\n' +
+              'OS 정보 : ' + window.AquaAxPlugin.DeviceOSDesc + '\n' +
+              '해상도 정보 : ' + window.AquaAxPlugin.DeviceMonitor + '\n' +
+              'IE 버전 : ' + window.AquaAxPlugin.DeviceIE + '\n'
+            );
+          }
+        }
+      });
+
+      window.onunload = function () {
+        if (window.setAxPlugin === true) window.AquaAxPlugin.FinalizeAuth();
+      };
     },
     // encparam 을 서버에서 생성하여 전달받는다.
     getEncodedParam: function () {
