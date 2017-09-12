d91fb7697b5a4e232ee77d56500112d7253209a9
ParkSeokje <molla4455@gmail.com>
2017-09-12 15:21:26 +0900

    아쿠아플레이어 적용중..

--- dist/javascripts/winpops/win_aquaplayer.js
+++ dist/javascripts/winpops/win_aquaplayer.js
@@ -1,2 +1,2 @@
-"use strict";window.requirejs(["common","aquaPlayerService"],function(e,a){var n=n||window.$;n(function(){var e={fileUrl:"http://pcst.aquan.dev.edu1004.kr/orangenamu/dev/cdnetworks.mp4",html:!0,userId:"test",container:n(".aquaplayer"),callback:function(){console.log("aqua service intialized")}};a=new a(e)})});
+"use strict";window.requirejs(["common","aquaPlayerService"],function(e,a){var n=n||window.$;n(function(){var e={fileUrl:"http://pcst.aquan.dev.edu1004.kr/orangenamu/dev/cdnetworks.mp4",html:!1,userId:"test",container:n(".aquaplayer"),callback:function(){console.log("aqua service intialized")}};a=new a(e)})});
 //# sourceMappingURL=../../maps/winpops/win_aquaplayer.js.map
