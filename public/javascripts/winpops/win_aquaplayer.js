fc6744f187ba7c4f071a753b49d5d59e0c37be29
ParkSeokje <molla4455@gmail.com>
2017-09-12 17:22:45 +0900

    아쿠아플레이어 적용중..

--- public/javascripts/winpops/win_aquaplayer.js
+++ public/javascripts/winpops/win_aquaplayer.js
@@ -10,7 +10,7 @@ window.requirejs(
     $(function () {
       var options = {
         fileUrl: 'http://pcst.aquan.dev.edu1004.kr/orangenamu/dev/cdnetworks.mp4',
-        html5: true,
+        html5: false,
         userId: 'test',
         container: $('.aquaplayer'),
         callback: function () {
