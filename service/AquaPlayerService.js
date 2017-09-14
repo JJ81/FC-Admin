d05368a2cf058211ea3c22270cee16f1502b7d4e
ParkSeokje <molla4455@gmail.com>
2017-09-14 18:33:20 +0900

    아쿠아플레이어 적용중..

--- service/AquaPlayerService.js
+++ service/AquaPlayerService.js
@@ -123,7 +123,7 @@ exports.show = (req, res, next) => {
               module_type: 'AquaPlayer',
               title: '아쿠아플레이어 Windows',
               video_url: res.locals.vodUrl + results[0][0].url,
-              watermark: 'test' // req.user.user_id
+              watermark: req.user.name
             });
           } else {
             return res.render('winpops/win_aquaplayer_html5', {
