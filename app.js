4307afbc5b080cabd29c863ae5fb0b2b085d1749
ParkSeokje <molla4455@gmail.com>
2017-10-20 15:45:23 +0900

    cors 적용중..

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
-app.use(cors);
+app.use(cors());
 
 app.use('/', routes);
 app.use('/dashboard', dashboard);
