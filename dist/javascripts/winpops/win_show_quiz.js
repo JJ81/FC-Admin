a77370c9c0e9197737559160586ccbdf59d2c67b
ParkSeokje <molla4455@gmail.com>
2017-12-12 19:01:26 +0900

    도움말 영상 적용중..

---
+++ dist/javascripts/winpops/win_show_quiz.js
@@ -0,0 +1,2 @@
+"use strict";requirejs(["common"],function(c){var i=$("#check-quiz"),n=$(".answer");i.bind("click",function(){n.show()})});
+//# sourceMappingURL=../../maps/winpops/win_show_quiz.js.map
