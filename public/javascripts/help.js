40565abdc466ed6977846106078475e55eac3c72
ParkSeokje <molla4455@gmail.com>
2017-12-12 16:39:35 +0900

    도움말 영상 적용중..

--- public/javascripts/help.js
+++ public/javascripts/help.js
@@ -2,15 +2,27 @@
 window.requirejs([ 'common' ],
 function (Util) {
   var $ = $ || window.$;
-  var $adminVideo = $('#admin_video');
-  // var $userVideo = $('#user_video');
+  var $video = $('#frame-video');
+  var $btnAdmin = $('#btn-admin');
+  var $btnUser = $('#btn-user');
 
   $(function () {
-    displayVideo();
+    displayAdminVideo();
   });
 
-  function displayVideo () {
-    $adminVideo.attr('src', '/api/v1/aqua-direct?os=' + Util.getOSName() + '&url=onm_오렌지러닝_관리자.mp4');
-    // $userVideo.attr('src', '/api/v1/aqua-direct?os=' + Util.getOSName() + '&url=onm_오렌지러닝_.mp4');
+  $btnAdmin.on('click', function () {
+    displayAdminVideo();
+  });
+
+  $btnUser.on('click', function () {
+    displayUserVideo();
+  });
+
+  function displayAdminVideo () {
+    $video.attr('src', '/api/v1/aqua-direct?os=' + Util.getOSName() + '&url=onm_오렌지러닝_관리자.mp4');
+  }
+
+  function displayUserVideo () {
+    $video.attr('src', '/api/v1/aqua-direct?os=' + Util.getOSName() + '&url=onm_오렌지러닝_.mp4');
   }
 });
